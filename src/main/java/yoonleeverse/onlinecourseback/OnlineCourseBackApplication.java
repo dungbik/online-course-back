41197ae2358b46c3cdc77b77c7dfca480f67630d
dungbik <q1w2e30630@gmail.com>
2022-04-03 00:15:40 +0900

    Feat: Initial Commit

---
+++ src/main/java/yoonleeverse/onlinecourseback/OnlineCourseBackApplication.java
@@ -0,0 +1,13 @@
+package yoonleeverse.onlinecourseback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineCourseBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineCourseBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/yoonleeverse/onlinecourseback/OnlineCourseBackApplicationTests.java
@@ -0,0 +1,13 @@
+package yoonleeverse.onlinecourseback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineCourseBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
