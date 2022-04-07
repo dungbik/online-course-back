e8f07c6b5ea76ea2753dc0e861b4bf44b294ce3d
dungbik <q1w2e30630@gmail.com>
2022-04-07 17:08:02 +0900

    Refactor: Move DataLoader into dataloaders folder

--- src/main/java/yoonleeverse/onlinecourseback/modules/course/resolver/dataloaders/MainTechDataLoaderWithContext.java
+++ src/main/java/yoonleeverse/onlinecourseback/modules/course/resolver/dataloaders/MainTechDataLoaderWithContext.java
@@ -1,4 +1,4 @@
-package yoonleeverse.onlinecourseback.modules.course.resolver;
+package yoonleeverse.onlinecourseback.modules.course.resolver.dataloaders;
 
 import com.netflix.graphql.dgs.DgsDataLoader;
 import lombok.RequiredArgsConstructor;
@@ -6,7 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.dataloader.BatchLoaderEnvironment;
 import org.dataloader.MappedBatchLoaderWithContext;
 import yoonleeverse.onlinecourseback.modules.course.service.CourseService;
-import yoonleeverse.onlinecourseback.modules.course.types.CourseType;
+import yoonleeverse.onlinecourseback.modules.course.types.TechType;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,15 +15,15 @@ import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 
-@DgsDataLoader(name = "prerequisite")
+@DgsDataLoader(name = "mainTechs")
 @RequiredArgsConstructor
 @Slf4j
-public class PrerequisiteDataLoaderWithContext implements MappedBatchLoaderWithContext<String, List<CourseType>> {
+public class MainTechDataLoaderWithContext implements MappedBatchLoaderWithContext<String, List<TechType>> {
 
     private final CourseService courseService;
 
     @Override
-    public CompletionStage<Map<String, List<CourseType>>> load(Set<String> keys, BatchLoaderEnvironment environment) {
-        return CompletableFuture.supplyAsync(() -> courseService.prerequisitesForCourses(new ArrayList<>(keys)));
+    public CompletionStage<Map<String, List<TechType>>> load(Set<String> keys, BatchLoaderEnvironment environment) {
+        return CompletableFuture.supplyAsync(() -> courseService.techsForCourses(new ArrayList<>(keys)));
     }
 }
