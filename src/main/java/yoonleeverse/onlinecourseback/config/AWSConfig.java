17dcf32c653a23f625c3afc99f1c27ee0a1d47f7
dungbik <q1w2e30630@gmail.com>
2022-04-25 16:27:44 +0900

    Feat: Add AWS S3 Config

---
+++ src/main/java/yoonleeverse/onlinecourseback/config/AWSConfig.java
@@ -0,0 +1,39 @@
+package yoonleeverse.onlinecourseback.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Slf4j
+public class AWSConfig {
+    @Value("${cloud.aws.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String secretKey;
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+    @Value("${cloud.aws.s3.bucket}")
+    public String s3Bucket;
+
+    @Bean
+    public AmazonS3Client amazonS3Client() {
+        log.info(String.format(
+                "AWS Config -> accessKey: %s, secretKey: %s, region: %s, s3Bucket: %s",
+                accessKey, secretKey, region, s3Bucket));
+
+        BasicAWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+        return (AmazonS3Client) AmazonS3ClientBuilder.standard()
+                .withRegion(region)
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .build();
+    }
+}
